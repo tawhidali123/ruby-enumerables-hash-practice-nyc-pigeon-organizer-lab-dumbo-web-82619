25c47c8f091ce0166039ad4d5456bd2a364a739f
Tawhid Ali <tawhidali126@gmail.com>
2019-08-14 20:36:45 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,39 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  holder = {}
+  
+  data.each {
+    |col_gen_liv, descriptions|
+    descriptions.each {
+      |value, array|
+      array.each {
+        |name|
+        if holder[name] == nil
+          holder[name] = {}
+          holder[name][col_gen_liv] = []
+        else holder[name][col_gen_liv] = []
+      end
+      }
+    }
+  }
+  
+  holder.each {
+    |name, values|
+    values.each {
+      |hash, array|
+      data.each {
+        |col_gen_liv, descriptions|
+        descriptions.each {
+          |value, array|
+          array.each {
+            |element|
+            if element == name && hash == col_gen_liv
+              holder[name][hash].push(value.to_s)
+            end
+          }
+        }
+      }
+    }
+  }
+  
+  return holder
 end
